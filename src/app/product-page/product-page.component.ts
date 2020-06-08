13bd0dc5b8c995d0d5eb987c97219cf992e3cdb9
anatoly <chesnokov.a@industry-soft.ru>
2020-06-08 16:42:32 +0300

    Add product page component

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../shared/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+  styleUrls: ['./product-page.component.scss']
 })
 export class ProductPageComponent implements OnInit {
 
-  constructor() { }
+  product$;
 
+  constructor(
+    private productServ: ProductService,
+    private route: ActivatedRoute
+  ) { }
   ngOnInit(): void {
+    this.product$ = this.route.params
+      .pipe( switchMap( params => {
+        return this.productServ.getById(params[`id`]);
+      }));
   }
-
 }
