a182e1604e2724de5188ccf67a9414b77ac9b199
anatoly <chesnokov.a@industry-soft.ru>
2020-06-08 17:46:56 +0300

    Add dashboard page component

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../shared/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  products = [];
+  pSub: Subscription;
+  rSub: Subscription;
+
+  constructor(
+    private productServ: ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.pSub = this.productServ.getAll().subscribe( products => {
+      console.log(products);
+      this.products = products;
+    });
   }
 
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
+
+    if (this.rSub) {
+      this.rSub.unsubscribe();
+    }
+  }
+
+  remove(id) {
+    this.rSub = this.productServ.remove(id).subscribe( () => {
+      this.products = this.products.filter( product => product.id !== id);
+    });
+  }
 }
